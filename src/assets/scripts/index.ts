a3754b60b0acc8279221e4d791b3d879f75f43c9
Aleksandr Solovei <solalex.s.a@gmail.com>
2023-10-14 19:00:36 +0300

    add menu

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -2,3 +2,8 @@ import { menuController } from "../../modules/Menu/Menu";
 import { lazyLoad } from "./utils";
 
 lazyLoad();
+
+const headerCatalogBtn = document.querySelector(".header__catalog");
+if (headerCatalogBtn) {
+  headerCatalogBtn.addEventListener("click", () => menuController.openMenu());
+}
--- src/modules/Menu/Menu.ts
+++ src/modules/Menu/Menu.ts
@@ -2,21 +2,37 @@ class MenuController {
   container: HTMLElement;
   items: NodeListOf<HTMLElement>;
   isActive: boolean;
+  bg: HTMLElement;
 
-  constructor(container: HTMLElement, items: NodeListOf<HTMLElement>) {
+  constructor(
+    container: HTMLElement,
+    bg: HTMLElement,
+    items: NodeListOf<HTMLElement>,
+  ) {
     this.container = container;
+    this.bg = bg;
     this.items = items;
     this.isActive = false;
 
     if (this.container) {
-      this.container.addEventListener("mouseenter", this.openMenu.bind(this));
-      this.container.addEventListener("mouseleave", this.closeMenu.bind(this));
+      this.container.addEventListener("click", (e) => {
+        e.stopPropagation();
+        this.openMenu.call(this);
+      });
+      // this.container.addEventListener("mouseleave", this.closeMenu.bind(this));
+      this.bg.addEventListener("click", (e) => {
+        e.stopPropagation();
+        this.closeMenu.call(this);
+      });
 
       items.forEach((i) => {
         const link = i.querySelector<HTMLElement>(".menu-item__link");
 
         if (link) {
-          link.addEventListener("mouseenter", this.openItem.bind(this, i));
+          link.addEventListener("click", (e) => {
+            e.stopPropagation();
+            this.openItem.call(this, i);
+          });
         }
       });
     }
@@ -53,6 +69,9 @@ class MenuController {
 
 const menu =
   document.querySelector<HTMLElement>(".menu") || document.createElement("div");
+const menuBG =
+  document.querySelector<HTMLElement>(".menu__bg") ||
+  document.createElement("div");
 const menuItems = document.querySelectorAll<HTMLElement>(".menu-item");
 
-export const menuController = new MenuController(menu, menuItems);
+export const menuController = new MenuController(menu, menuBG, menuItems);
